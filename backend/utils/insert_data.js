31491de044087a52c45da2c99c2eabfd7aeaf98f
Aleix Casas Barco <70470419+aleixcaasas@users.noreply.github.com>
2023-11-08 19:55:33 +0100

    Recipe ingredients parsing and insert in firebase
    * Add scripts to insert data into Firestore and
    parse recipe ingredients
    
    * Fix batch commit issue and remove unused file
    
    * Recipe ingredients parsing finished
    
    * Add scripts to insert data into Firestore and
    parse recipe ingredients
    
    * Fix batch commit issue and remove unused file
    
    * Recipe ingredients parsing finished

---
+++ backend/utils/insert_data.js
@@ -0,0 +1,33 @@
+const admin = require('firebase-admin')
+const serviceAccount = require('../firebase-key.json')
+const recipes = require('../../data/recipes.json')
+const ingredients = require('../../data/ingredients.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
+
+const db = admin.firestore()
+
+const insertIngredients = async () => {
+    const batch = db.batch()
+    ingredients.forEach((ingredient) => {
+        const docId = ingredient.id.toString()
+        const docIngredient = db.collection('ingredients').doc(docId)
+        batch.set(docIngredient, ingredient)
+    })
+    result = await batch.commit()
+}
+
+const insertRecipes = async () => {
+    const batch = db.batch()
+    recipes.forEach((recipe) => {
+        const docId = recipe.id.toString()
+        const docRecipe = db.collection('recipes').doc(docId)
+        batch.set(docRecipe, recipe)
+    })
+    result = await batch.commit()
+}
+
+insertIngredients()
+insertRecipes()
