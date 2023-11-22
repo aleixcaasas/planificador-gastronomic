145a3765c3e67c2107fd703de8782ee9e07d7de5
aleixcaasas <acasasbarco@gmail.com>
2023-11-22 23:47:33 +0100

    Update frontend index.html and NavBar component

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,3 +1,129 @@
-function NavBar() {
-    return <></>
+import { useUser } from '../../context/UserContext'
+import { useState } from 'react'
+
+export function NavBar() {
+    const { user, isAuthenticated } = useUser()
+
+    const [isUserDropdownOpen, setUserDropdownOpen] = useState(false)
+    const [isMenuDropdownOpen, setMenuDropdownOpen] = useState(false)
+
+    const toggleUserDropdown = () => {
+        setUserDropdownOpen(!isUserDropdownOpen)
+        setMenuDropdownOpen(false)
+    }
+    const toggleMenuDropdown = () => {
+        setMenuDropdownOpen(!isMenuDropdownOpen)
+        setUserDropdownOpen(false)
+    }
+
+    return (
+        <nav className='bg-white border-gray-200'>
+            <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+                <a href='/' className='flex items-center space-x-3 rtl:space-x-reverse'>
+                    <img src='src\assets\favicon.png' className='h-8' />
+                    <span className='self-center text-2xl font-semibold whitespace-nowrap'>Menu Vital</span>
+                </a>
+                <div className='flex items-center space-x-3 md:space-x-0 rtl:space-x-reverse'>
+                    <button
+                        type='button'
+                        className='flex text-sm bg-gray-800 rounded-full focus:ring-2 focus:ring-false-orange'
+                        onClick={toggleUserDropdown}
+                    >
+                        <img
+                            className='w-8 h-8 rounded-full'
+                            src={user.photoURL || 'https://cdn-icons-png.flaticon.com/512/666/666201.png'}
+                            alt='user photo'
+                        />
+                    </button>
+                    <button
+                        data-collapse-toggle='navbar-user'
+                        type='button'
+                        className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-false-orange '
+                        onClick={toggleMenuDropdown}
+                    >
+                        <svg
+                            className='w-5 h-5'
+                            aria-hidden='true'
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 17 14'
+                        >
+                            <path
+                                stroke='currentColor'
+                                stroke-linecap='round'
+                                stroke-linejoin='round'
+                                stroke-width='2'
+                                d='M1 1h15M1 7h15M1 13h15'
+                            />
+                        </svg>
+                    </button>
+                </div>
+                <div
+                    className={`absolute left-1/2 top-10 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-md z-50 ${
+                        !isUserDropdownOpen ? 'hidden' : ''
+                    }`}
+                    id='user-dropdown'
+                >
+                    <div className='px-4 py-3'>
+                        <span className='block text-sm text-gray-900 '>
+                            <b>{user.name || 'Aleix Casas'}</b>
+                        </span>
+                        <span className='block text-sm  text-gray-500 truncate '>{user.email}</span>
+                    </div>
+                    <ul className='p-2' aria-labelledby='user-menu-button'>
+                        <li className='hover:bg-false-white rounded-lg'>
+                            <a href='#' className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'>
+                                Perfil
+                            </a>
+                        </li>
+                        <li className='hover:bg-false-white rounded-lg'>
+                            <a href='#' className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'>
+                                Cerrar sesión
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div
+                    className={`items-center justify-between w-full ${!isMenuDropdownOpen ? 'hidden' : ''}`}
+                    id='menu-dropdown'
+                >
+                    <ul className='flex flex-col font-medium p-2 mt-4 border border-false-light-gray shadow-md rounded-lg rtl:space-x-reverse '>
+                        <li className='hover:bg-false-white rounded-lg' aria-current='page'>
+                            <a
+                                href='#'
+                                className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
+                                aria-current='page'
+                            >
+                                Inicio
+                            </a>
+                        </li>
+                        <li className='hover:bg-false-white rounded-lg'>
+                            <a
+                                href='#'
+                                className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
+                            >
+                                Mis Recetas
+                            </a>
+                        </li>
+                        <li className='hover:bg-false-white rounded-lg'>
+                            <a
+                                href='#'
+                                className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
+                            >
+                                Lista de la compra
+                            </a>
+                        </li>
+                        <li className='hover:bg-false-white rounded-lg'>
+                            <a
+                                href='#'
+                                className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
+                            >
+                                Explorar recetas
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import LoginRegister from '../components/LoginRegister/LoginRegister'
 import Planification from '../components/Planification/Planification'
+import { NavBar } from '../components/NavBar/NavBar.jsx'
 import { useUser } from '../context/UserContext.jsx'
 
 function Home() {
     const { user, isAuthenticated } = useUser()
 
-    return isAuthenticated ? <Planification /> : <LoginRegister />
+    return isAuthenticated ? (
+        <>
+            <NavBar />
+            <Planification />
+        </>
+    ) : (
+        <LoginRegister />
+    )
 }
 
 export default Home
