eb9824f23d8b8c8c06617e4841880a87cc85b617
aleixcaasas <acasasbarco@gmail.com>
2024-01-03 20:10:04 +0100

    Mis recetas + Explorar recetas

--- backend/routes/recipe.routes.js
+++ backend/routes/recipe.routes.js
@@ -1,11 +1,18 @@
 const Router = require('express')
-const { getRecipes, addMeal, addFavourite, removeFavourite } = require('../controllers/recipe.controller.js')
+const {
+    getRecipes,
+    getUserRecipes,
+    createRecipe,
+    addFavourite,
+    removeFavourite
+} = require('../controllers/recipe.controller.js')
 
 const recipeRouter = Router()
 
 recipeRouter.get('/recipes', getRecipes)
-recipeRouter.post('/new-recipe', addMeal)
+recipeRouter.post('/new-recipe', createRecipe)
 recipeRouter.post('/add-favourite', addFavourite)
 recipeRouter.post('/remove-favourite', removeFavourite)
+recipeRouter.post('/user-recipes', getUserRecipes)
 
 module.exports = { recipeRouter }
--- frontend/src/pages/ExplorarRecetas.jsx
+++ frontend/src/pages/ExplorarRecetas.jsx
@@ -1,13 +1,15 @@
 import { NavBar } from '../components/NavBar/NavBar.jsx'
 import { useUser } from '../context/UserContext.jsx'
+import ExploreRecipes from '../components/ExploreRecipes/ExploreRecipes.jsx'
 
 function ExplorarRecetas() {
     return (
         <>
-            <div className='w-[29.375rem] h-[29.375rem] left-[-3.5rem] top-[40rem] absolute bg-false-blue bg-opacity-70 rounded-full -z-10' />
-            <div className='w-[31.25rem] h-[31.25rem] left-[-12.5rem] top-[15.625rem] absolute bg-false-blue bg-opacity-30 rounded-full -z-10' />
-            <div className='w-[26.25rem] h-[26.25rem] left-[3.1875rem] top-[0.625rem] absolute bg-false-blue bg-opacity-70 rounded-full -z-10' />
+            <div className='w-[29.375rem] h-[29.375rem] left-[-3.5rem] top-[40rem] bg-false-blue bg-opacity-70 rounded-full -z-10 fixed' />
+            <div className='w-[31.25rem] h-[31.25rem] left-[-12.5rem] top-[15.625rem] fixed bg-false-blue bg-opacity-30 rounded-full -z-10' />
+            <div className='w-[26.25rem] h-[26.25rem] left-[3.1875rem] top-[0.625rem] fixed bg-false-blue bg-opacity-70 rounded-full -z-10' />
             <NavBar />
+            <ExploreRecipes />
         </>
     )
 }
